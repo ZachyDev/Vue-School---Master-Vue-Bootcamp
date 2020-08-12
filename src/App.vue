7d7a13bb1acfe18e7167c36d440cea3a1497c663
ZachyDev <mosetizachary001@gmail.com>
2020-08-12 12:54:11 +0300

    Vue school interactive vue bootcamp❇❇❇

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home';
+export default {
+  name: "App",
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+
+</style>
